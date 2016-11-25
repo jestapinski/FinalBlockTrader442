40a9df910072ab1c3180affcd410736945281527
Jordan Stapinski <jstapins@andrew.cmu.edu>
2016-11-25 16:42:33 -0500

    Initial Commit of rebuild

--- BlockTrader/ViewController.swift
+++ BlockTrader/ViewController.swift
@@ -7,12 +7,31 @@
 //
 
 import UIKit
+import FacebookLogin
+import FacebookCore
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let loginButton = LoginButton(readPermissions: [ .publicProfile, .email ])
+        loginButton.center = view.center
+        
+        view.addSubview(loginButton)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        print(AccessToken.current)
+        if let accessToken = AccessToken.current{
+            print("HERE!")
+            self.moveToMainPage(accessToken: accessToken)
+        }
+    }
+    
+    func moveToMainPage(accessToken: AccessToken){
+        let mainStoryboard = UIStoryboard(name: "Main", bundle: Bundle.main)
+        let vc : UIViewController = mainStoryboard.instantiateViewController(withIdentifier: "loggedin") as! MainPageViewController
+        self.present(vc, animated: true, completion: nil)
     }
 
     override func didReceiveMemoryWarning() {
---
+++ Pods/Target Support Files/Pods-BlockTrader/Pods-BlockTrader-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BlockTrader : NSObject
+@end
+@implementation PodsDummy_Pods_BlockTrader
+@end
